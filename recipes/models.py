1cd344e0fca3011c56b0ba11de113a1a56ecf1e7
nick-rebrik <77996995+nick-rebrik@users.noreply.github.com>
2022-02-17 11:20:37 +0100

    initial & added models

---
+++ recipes/models.py
@@ -0,0 +1,26 @@
+from django.db import models
+
+
+class User(models.Model):
+    first_name = models.CharField('First name', max_length=250)
+    last_name = models.CharField('Last name', max_length=250)
+
+    class Meta:
+        ordering = ('name',)
+        verbose_name = 'User'
+        verbose_name_plural = 'Users'
+
+    def __str__(self):
+        return f'{self.first_name} {self.last_name}'
+
+
+class Recipe(models.Model):
+    title = models.CharField('Title', max_length=250)
+    description = models.TextField('Description')
+    image = models.ImageField('Image', upload_to='images/')
+    user = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        related_name='recipes'
+    )
+    view_count = models.PositiveIntegerField('View count', default=0)
